4bba9fc16575e9d875cc97ce2a94fdcc4b3d7d23
will2357 <will.highducheck@gmail.com>
2020-04-06 23:03:15 -0600

    final exercises in classification

--- ml_techniques/01_supervised_scikit/1_classification/labs.py
+++ ml_techniques/01_supervised_scikit/1_classification/labs.py
@@ -141,11 +141,90 @@ new_prediction = knn.predict(X_new)
 print("Prediction: {}".format(new_prediction))
 
 
+################################################################################
+################### Ex. 10: k-Nearest Neighbors: Predict #######################
+################################################################################
+
+# Import necessary modules
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+# Load the digits dataset: digits
+digits = datasets.load_digits()
+
+# Print the keys and DESCR of the dataset
+print(digits.keys())
+print(digits.DESCR)
+
+# Print the shape of the images and data keys
+print(digits.images.shape)
+print(digits.data.shape)
+
+# Display digit 1010
+plt.imshow(digits.images[1010], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
+
+
+################################################################################
+############## Ex. 11: Train/Test Split + Fit/Predict/Accuracy #################
+################################################################################
+
+# Import necessary modules
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+# Create feature and target arrays
+X = digits.data
+y = digits.target
+
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, #defaults tp 0.25
+                                                    random_state=42, stratify=y)
+
+# Create a k-NN classifier with 7 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors=7)
+
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))
+
+
+################################################################################
+#################### Ex. 12: Overfitting and underfitting ######################
+################################################################################
+
+
+# Setup arrays to store train and test accuracies
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over different values of k
+for i, k in enumerate(neighbors):
+    # Setup a k-NN Classifier with k neighbors: knn
+    knn = KNeighborsClassifier(n_neighbors=k)
+
+    # Fit the classifier to the training data
+    knn.fit(X_train, y_train)
+
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(X_train, y_train)
+
+    #Compute accuracy on the testing set
+    test_accuracy[i] = knn.score(X_test, y_test)
+
+# Generate plot
+plt.title('k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
 
 ################################################################################
 ################################### Run: #######################################
 ##### ipython ml_techniques/01_supervised_scikit/1_classification/labs.py ######
 ################################################################################
-
-
-
