dd1cccba90c0ddde145ec9d72dfdbe4fcdcd1b08
will2357 <will.highducheck@gmail.com>
2020-04-06 22:15:17 -0600

    Ex. 8 with data setup

--- ml_techniques/01_supervised_scikit/1_classification/labs.py
+++ ml_techniques/01_supervised_scikit/1_classification/labs.py
@@ -80,7 +80,7 @@ df.info()
 df.describe()
 
 ################################################################################
-########################### Ex. 4: Graphical EDA ###############################
+######################### Ex. 5: Graphical EDA #################################
 ################################################################################
 
 plt.figure() # Start new plot
@@ -92,6 +92,55 @@ plt.figure() # Start new plot
 sns.countplot(x='missile', hue='party', data=df, palette='RdBu')
 plt.xticks([0,1], ['No', 'Yes'])
 plt.show()
+
+
+################################################################################
+#################### Ex. 7: k-Nearest Neighbors: Fit ###########################
+################################################################################
+
+# Import KNeighborsClassifier from sklearn.neighbors
+from sklearn.neighbors import KNeighborsClassifier
+
+# Create arrays for the features and the response variable
+y = df['party'].values
+X = df.drop('party', axis=1).values
+
+# Create a k-NN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X, y)
+
+################################################################################
+################### Ex. 8: k-Nearest Neighbors: Predict ########################
+################################################################################
+
+X_new = pd.DataFrame([[0.696469, 0.286139, 0.226851, 0.551315, 0.719469,
+                       0.423106, 0.980764, 0.68483, 0.480932, 0.392118,
+                       0.343178, 0.72905, 0.438572, 0.059678, 0.398044,
+                       0.737995]])
+
+# Import KNeighborsClassifier from sklearn.neighbors
+from sklearn.neighbors import KNeighborsClassifier
+
+# Create arrays for the features and the response variable
+y = df['party'].values
+X = df.drop('party', axis=1).values
+
+# Create a k-NN classifier with 6 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X, y)
+
+# Predict the labels for the training data X
+y_pred = knn.predict(X)
+
+# Predict and print the label for the new data point X_new
+new_prediction = knn.predict(X_new)
+print("Prediction: {}".format(new_prediction))
+
+
 
 ################################################################################
 ################################### Run: #######################################
