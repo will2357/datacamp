98a9eb7b74c9b60e3730fc90d0966d61b2c826a7
will2357 <will.highducheck@gmail.com>
2020-04-08 21:08:31 -0600

    Deep Learning in Python: Chapters 1 and 2: Basics and Optimization/Backward Propagation

--- ml_techniques/02_deep_learning/01_basics/labs.py
+++ ml_techniques/02_deep_learning/01_basics/labs.py
@@ -1,12 +1,7 @@
 import os
 
 # Course deps
-# from sklearn import datasets
-# import pandas as pd
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# plt.style.use('ggplot')
+import numpy as np
 
 # Open shared user defined functions
 udf = "ml_techniques/02_deep_learning/udfs.py"
@@ -16,8 +11,143 @@ exec(open(os.path.abspath(udf)).read())
 # np.random.seed(42)
 
 ################################################################################
-############################## Ex. X: YZ #######################################
+############## Ex. 4: Coding the forward propagation algorithm #################
 ################################################################################
+input_data = np.array([3, 5])
+weights = {'node_0': np.array([2, 4]), 'node_1': np.array([ 4, -5]),
+           'output': np.array([2, 7])}
+
+# Calculate node 0 value: node_0_value
+node_0_value = (input_data * weights['node_0']).sum()
+
+# Calculate node 1 value: node_1_value
+node_1_value = (input_data * weights['node_1']).sum()
+
+# Put node values into array: hidden_layer_outputs
+hidden_layer_outputs = np.array([node_0_value, node_1_value])
+
+# Calculate output: output
+output = (hidden_layer_outputs * weights['output']).sum()
+
+# Print output
+print(output)
+
+
+################################################################################
+############## Ex. 6: The Rectified Linear Activation Function #################
+################################################################################
+
+def relu(input):
+    '''Define your relu activation function here'''
+    # Calculate the value for the output of the relu function: output
+    output = max(0, input)
+
+    # Return the value just calculated
+    return(output)
+
+# Calculate node 0 value: node_0_output
+node_0_input = (input_data * weights['node_0']).sum()
+node_0_output = relu(node_0_input)
+
+# Calculate node 1 value: node_1_output
+node_1_input = (input_data * weights['node_1']).sum()
+node_1_output = relu(node_1_input)
+
+# Put node values into array: hidden_layer_outputs
+hidden_layer_outputs = np.array([node_0_output, node_1_output])
+
+# Calculate model output (do not apply relu)
+model_output = (hidden_layer_outputs * weights['output']).sum()
+
+# Print model output
+print(model_output)
+
+################################################################################
+######## Ex. 6: Applying the network to many observations/rows of data #########
+################################################################################
+
+weights = {'node_0': np.array([2, 4]), 'node_1': np.array([ 4, -5]),
+           'output': np.array([2, 7])}
+
+input_data = [np.array([3, 5]), np.array([ 1, -1]), np.array([0, 0]),
+              np.array([8, 4])]
+
+# Define predict_with_network()
+def predict_with_network(input_data_row, weights):
+
+    # Calculate node 0 value
+    node_0_input = (input_data_row * weights['node_0']).sum()
+    node_0_output = relu(node_0_input)
+
+    # Calculate node 1 value
+    node_1_input = (input_data_row * weights['node_1']).sum()
+    node_1_output = relu(node_1_input)
+
+    # Put node values into array: hidden_layer_outputs
+    hidden_layer_outputs = np.array([node_0_output, node_1_output])
+
+    # Calculate model output
+    input_to_final_layer = (hidden_layer_outputs * weights['output']).sum()
+    model_output = relu(input_to_final_layer)
+
+    # Return model output
+    return(model_output)
+
+
+# Create empty list to store prediction results
+results = []
+for input_data_row in input_data:
+    # Append prediction to results
+    results.append(predict_with_network(input_data_row, weights))
+
+# Print results
+print(results)
+
+
+################################################################################
+##################### Ex. 10: Multi-layer neural networks ######################
+################################################################################
+
+
+input_data = np.array([3, 5])
+
+weights = {'node_0_0': np.array([ 2,  4]),
+           'node_0_1': np.array([ 4, -5]),
+           'node_1_0': np.array([-1,  2]),
+           'node_1_1': np.array([ 1,  2]),
+           'output':   np.array([ 2,  7])}
+
+def predict_with_network(input_data):
+    # Calculate node 0 in the first hidden layer
+    node_0_0_input  = (input_data * weights['node_0_0']).sum()
+    node_0_0_output = relu(node_0_0_input)
+
+    # Calculate node 1 in the first hidden layer
+    node_0_1_input  = (input_data * weights['node_0_1']).sum()
+    node_0_1_output = relu(node_0_1_input)
+
+    # Put node values into array: hidden_0_outputs
+    hidden_0_outputs = np.array([node_0_0_output, node_0_1_output])
+
+    # Calculate node 0 in the second hidden layer
+    node_1_0_input  = (hidden_0_outputs * weights['node_1_0']).sum()
+    node_1_0_output = relu(node_1_0_input)
+
+    # Calculate node 1 in the second hidden layer
+    node_1_1_input  = (hidden_0_outputs * weights['node_1_1']).sum()
+    node_1_1_output = relu(node_1_1_input)
+
+    # Put node values into array: hidden_1_outputs
+    hidden_1_outputs = np.array([node_1_0_output, node_1_1_output])
+
+    # Calculate model output: model_output
+    model_output = (hidden_1_outputs * weights['output']).sum()
+
+    # Return model_output
+    return(model_output)
+
+output = predict_with_network(input_data)
+print(output)
 
 
 ################################################################################
